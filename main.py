5244ffca0476231b59bb6519805a9e192af373d7
Fábio Marques <fabiomarquescbf@gmail.com>
2024-02-06 20:22:05 -0300

    info_Notas
    Informações necessárias da nota.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def pegar_infos(nome_arquivo):
     print(f"Pegou as informações {arquivo}")
     with open(f'nfs/{nome_arquivo}', "rb") as arquivo_xml:
         dic_arquivo = xmltodict.parse(arquivo_xml)
-     #  print(json.dumps(dic.arquivo, {indent=4}))
+       # print(json.dumps(dic.arquivo, indent=4))
         infos_nf = dic_arquivo["NFe"]["infNFe"]
         numero_nota = infos_nf["@Id"]
         empresa_emissora = infos_nf['emit']['xNome']
@@ -14,12 +14,7 @@ def pegar_infos(nome_arquivo):
         endereco = infos_nf["dest"]["enderDest"]
         peso = infos_nf["transp"]["vol"]["pesoB"]
         print(numero_nota, empresa_emissora, nome_cliente, endereco , peso, sep="\n")
-
-
-
-
-
-
+        
 
 lista_arquivos = os.listdir("nfs")
 print(lista_arquivos)
