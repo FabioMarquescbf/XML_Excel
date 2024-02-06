4888d487da3ede06267ae858b34c4692b3da254c
Fábio Marques <fabiomarquescbf@gmail.com>
2024-02-06 20:19:45 -0300

    xml <-> dicionario
    Listando os arquivos xml ; Transformando xml em dicionario ;
    Percorrendo as informações ;

--- main.py
+++ main.py
@@ -1,32 +1,30 @@
 import xmltodict
 import os
-import pandas as pd
+import json
 
-def pegar_infos(nome_arquivo, valores):
-    # print(f"Pegou as informações {nome_arquivo}")
+def pegar_infos(nome_arquivo):
+    print(f"Pegou as informações {arquivo}")
     with open(f'nfs/{nome_arquivo}', "rb") as arquivo_xml:
         dic_arquivo = xmltodict.parse(arquivo_xml)
-
-        if "NFe" in dic_arquivo:
-            infos_nf = dic_arquivo["NFe"]['infNFe']
-        else:
-            infos_nf = dic_arquivo['nfeProc']["NFe"]['infNFe']
+     #  print(json.dumps(dic.arquivo, {indent=4}))
+        infos_nf = dic_arquivo["NFe"]["infNFe"]
         numero_nota = infos_nf["@Id"]
         empresa_emissora = infos_nf['emit']['xNome']
         nome_cliente = infos_nf["dest"]["xNome"]
         endereco = infos_nf["dest"]["enderDest"]
-        if "vol" in infos_nf["transp"]:
-            peso = infos_nf["transp"]["vol"]["pesoB"]
-        else:
-            peso = "Não informado"
-        valores.append([numero_nota, empresa_emissora, nome_cliente, endereco, peso])
+        peso = infos_nf["transp"]["vol"]["pesoB"]
+        print(numero_nota, empresa_emissora, nome_cliente, endereco , peso, sep="\n")
+
+
+
+
+
+
 
 lista_arquivos = os.listdir("nfs")
+print(lista_arquivos)
 
-colunas = ["numero_nota", "empresa_emissora", "nome_cliente", "endereco", "peso"]
-valores = []
 for arquivo in lista_arquivos:
-    pegar_infos(arquivo, valores)
-
-tabela = pd.DataFrame(columns=colunas, data=valores)
-tabela.to_excel("NotasFiscais.xlsx", index=False)
+    pegar_infos(arquivo)
+    break
+    
