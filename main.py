283f0ca3c0af98b803bac7068f300e487420a337
Fábio Marques <fabiomarquescbf@gmail.com>
2024-02-06 20:44:24 -0300

    Obtendo Infos
    Obtendo as informações de todas as NFs + Correção de erros

--- main.py
+++ main.py
@@ -6,20 +6,31 @@ def pegar_infos(nome_arquivo):
     print(f"Pegou as informações {arquivo}")
     with open(f'nfs/{nome_arquivo}', "rb") as arquivo_xml:
         dic_arquivo = xmltodict.parse(arquivo_xml)
-       # print(json.dumps(dic.arquivo, indent=4))
-        infos_nf = dic_arquivo["NFe"]["infNFe"]
+
+    try:
+        if "NFe" in dic_arquivo:
+            infos_nf = dic_arquivo["NFe"]['infNFe']
+        else:
+            infos_nf = dic_arquivo['nfeProc']["NFe"]["infNFe"]
         numero_nota = infos_nf["@Id"]
         empresa_emissora = infos_nf['emit']['xNome']
         nome_cliente = infos_nf["dest"]["xNome"]
         endereco = infos_nf["dest"]["enderDest"]
-        peso = infos_nf["transp"]["vol"]["pesoB"]
+        if "vol" in infos_nf["transp"]:
+            peso = infos_nf["transp"]["vol"]["pesoB"]
+        else:
+            peso = "Nãi informado"
         print(numero_nota, empresa_emissora, nome_cliente, endereco , peso, sep="\n")
-        
+    except Exception as e:
+        print(e)
+        print(json.dumps(dic_arquivo, indent=4))
+
+
 
 lista_arquivos = os.listdir("nfs")
 print(lista_arquivos)
 
 for arquivo in lista_arquivos:
     pegar_infos(arquivo)
-    break
+   # break
     
